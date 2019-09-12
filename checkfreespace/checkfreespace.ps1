0cf505087fde43bbaab8e38367b2392dd8a23f33
Lee Starr <theleestarr@users.noreply.github.com>
2019-09-12 03:22:25 -0700

    Update checkfreespace.ps1

--- checkfreespace/checkfreespace.ps1
+++ checkfreespace/checkfreespace.ps1
@@ -1,6 +1,9 @@
 # -----------------------------------------------------------------------------############################
 # Script Function: This sample Windows powershell script calculates free disk spaces
 # in multiple servers and emails copy of  csv report. 
+# Author: Joel Souza 
+# Date: 06/20/2015 
+# 
 # ----------------------------------------------------------------------------- ############################
 $erroractionpreference = "SilentlyContinue"
 
@@ -33,14 +36,14 @@ $LogDate = get-date -f yyyyMMddhhmm
 #Define location of text file containing your servers. It might also
 #be stored on a local file system for example, D:\ServerStorageReport\DiskReport
 
-$File = Get-Content -Path C:\Scripts\DiskReport\CheckFreeSpace\Servers.txt
+$File = Get-Content -Path E:\lhaarmann\DiskReport\CheckFreeSpace\Servers.txt
 
 #Define admin account variable (Uncommented it and the line in Get-WmiObject
 #Line 44 below is commented out because I run this script via task schedule.
 #If you wish to run this script manually, please uncomment line 44.
 
 
-$RunAccount = get-Credential  
+#$RunAccount = get-Credential  
 
 #the disk $DiskReport variable checks all servers returned by the $File variable (line 37)
 
@@ -67,42 +70,17 @@ $DiskReport |
 
 Select-Object @{Label = "Server Name";Expression = {$_.SystemName}},
 @{Label = "Drive Letter";Expression = {$_.DeviceID}},
-@{Label = "Total Capacity (GB)";Expression = {"{0:N1}" -f( $_.Size / 1gb)}},
-@{Label = "Free Space (GB)";Expression = {"{0:N1}" -f( $_.Freespace / 1gb ) }},
-@{Label = 'Free Space (%)'; Expression = {"{0:P0}" -f ($_.freespace/$_.size)}} |
+@{Label = 'SizeInGB';Expression={[math]::round($_.Size / 1GB,2)}},
+@{Label = 'FreeSpaceInGB';Expression={[math]::round($_.FreeSpace / 1GB,2)}},
+@{Label = 'FreeSpaceIn%';Expression={[math]::round($_.FreeSpace / $_.Size * 100,2)}},
+@{Label = 'FreeSpaceLessThan10%';Expression={If(($_.FreeSpace / $_.Size * 100) -lt 10){$true}Else{$false}}}|
 
 
 #Export report to CSV file (Disk Report)
 
-Export-Csv -path "C:\Scripts\DiskReport\CheckFreeSpace\Reports\RHB_Windows_DiskReport_$logDate.csv" -NoTypeInformation
+Export-Csv -path "E:\lhaarmann\DiskReport\CheckFreeSpace\Reports\RHB_Windows_DiskReport_$logDate.csv" -NoTypeInformation
 
-
-
-#Send disk report using the exchange email module
-
-$fromaddress = "WindowsDriveReport@rhb.ad" 
-$toaddress = "lee.haarmann@changehealthcare.com" 
-$CCaddress = "" 
-$Subject = "Windows Server Free Space Report" 
-$body = get-content "C:\Scripts\DiskReport\CheckFreeSpace\content.htm" 
-$attachment = (Get-ChildItem C:\Scripts\DiskReport\CheckFreeSpace\Reports\*.* | sort LastWriteTime | select -last 1)
-$smtpserver = "sjmgmail.rhb.ad"
-
-#################################### 
- 
-#$message = new-object System.Net.Mail.MailMessage 
-#$message.From = $fromaddress 
-#$message.To.Add($toaddress) 
-#$message.CC.Add($CCaddress)  
-#$message.IsBodyHtml = $True 
-#$message.Subject = $Subject 
-#$attach = new-object Net.Mail.Attachment($attachment) 
-#$message.Attachments.Add($attach) 
-#$message.body = $body 
-#$smtp = new-object Net.Mail.SmtpClient($smtpserver) 
-#$smtp.Send($message) 
-
-
-
+#Send-MailMessage
+Send-MailMessage -From 'canoc@changehealthcare.com' -To 'lee.haarmann@changehealthcare.com' -Attachment (Get-ChildItem E:\lhaarmann\DiskReport\CheckFreeSpace\Reports\*.* | sort LastWriteTime | select -last 1) -Subject 'Disk Space Check' -SmtpServer 'SJMGMAIL.RHF.AD'
 
 ## END OF SCRIPT ###
